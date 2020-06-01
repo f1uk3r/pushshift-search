b1929b6eb802d9b852bf68c412908bd5dc131290
f1uk3r <agarwal.umang4321@gmail.com>
2020-06-01 16:57:58 +0530

    recieving data from get request

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let searchedArray = []
-let apiUrlForSubmissions = "https://api.pushshift.io/reddit/search/submission/?"
-let apiUrlForComments = "https://api.pushshift.io/reddit/search/comment/?"
+let apiUrlForSubmissions = "https://api.pushshift.io/reddit/search/submission/?size=10000&sort=desc&sort_type=score&filter=selftext,title,num_comments,score,url,author,created_utc"
+let apiUrlForComments = "https://api.pushshift.io/reddit/search/comment/?size=10000&sort=desc&sort_type=score&filter=body,score,permalink,author,created_utc"
 const form = document.querySelector('form')
 form.addEventListener('submit', event => {
     event.preventDefault();
@@ -12,41 +12,40 @@ function apiUrlBuilder() {
     let subreddit = document.querySelector('#subreddit').value
     let user = document.querySelector('#user').value
     if (searchTerm.length !== 0) {
-        apiUrlForSubmissions += "q=" + searchTerm;
-        apiUrlForComments += "q=" + searchTerm;
-        if (subreddit.length !== 0) {
-            apiUrlForSubmissions += "&subreddit=" + subreddit;
-            apiUrlForComments += "&subreddit=" + subreddit;
-        }
-        if (user.length !== 0) {
-            apiUrlForSubmissions += "&author=" + user;
-            apiUrlForComments += "&author=" + user;
-        }
+        apiUrlForSubmissions += "&q=" + searchTerm;
+        apiUrlForComments += "&q=" + searchTerm;
+    }
+    if (subreddit.length !== 0) {
+        apiUrlForSubmissions += "&subreddit=" + subreddit;
+        apiUrlForComments += "&subreddit=" + subreddit;
+    }
+    if (user.length !== 0) {
+        apiUrlForSubmissions += "&author=" + user;
+        apiUrlForComments += "&author=" + user;
+    }
+    requestApi(apiUrlForSubmissions)
+    /*Http.onreadystatechange=(e)=>{
+        let responseObject = Http.response
+        console.log(responseObject)
+        console.log(responseData)
+    }*/
+}
 
-    } else {
-        if (subreddit.length !== 0) {
-            apiUrlForSubmissions += "subreddit=" + subreddit;
-            apiUrlForComments += "subreddit=" + subreddit;
-            if (user.length !== 0) {
-                apiUrlForSubmissions += "&author=" + user;
-                apiUrlForComments += "&author=" + user;
-            }
-        } else {
-            if (user.length !== 0) {
-                apiUrlForSubmissions += "author=" + user;
-                apiUrlForComments += "author=" + user;
-            }
+function requestApi(url) {
+    const Http = new XMLHttpRequest()
+    Http.open("GET", url)
+    Http.responseType = 'json'
+    Http.send()
+    Http.onload = function ()  {
+        let status = Http.status
+        if (status === 200) {
+            let responseObject = Http.response
+            let responseData = responseObject["data"]
+            showResponse(responseData)
         }
     }
-    const Http = new XMLHttpRequest()
-    Http.open("GET", apiUrlForSubmissions)
-    Http.send()
-    Http.onreadystatechange=(e)=>{
-        console.log(Http.responseText)
-    }
-    /* let aBook = new Book(title, author, pages, read)
-    myLibrary.push(aBook)
-    let tableBody = document.querySelector('tBody');
-    tableBody.innerHTML = ''
-    render() */
+}
+
+function showResponse(data) {
+    
 }
